aff3995e9cbcf756c651db0794f7c1d9f29ec555
Chad Stabler <stablerpsn@gmail.com>
2022-04-25 17:34:29 -0500

    grabbed result elements that I forgot earlier, more work on event listener for button1

--- app.js
+++ app.js
@@ -6,16 +6,32 @@ const button3 = document.getElementById('btn3');
 const shell1 = document.getElementById('shell1');
 const shell2 = document.getElementById('shell2');
 const shell3 = document.getElementById('shell3');
+
+const wins = document.getElementById('win');
+const losses = document.getElementById('loss');
+const total = document.getElementById('total');
 // let state
 let win = 0;
 let loss = 0;
 // set event listeners
 button1.addEventListener('click', () => {
-  shell1.classList.remove('reveal');
-  shell2.classList.remove('reveal');
-  shell3.classList.remove('reveal');
+    shell1.classList.remove('reveal');
+    shell2.classList.remove('reveal');
+    shell3.classList.remove('reveal');
 
-  const randomShell = Math.ceil(Math.random() * 3);
+    const randomShell = Math.ceil(Math.random() * 3);
+
+    if (randomShell === 1) {
+        shell1.classList.add('reveal');
+        win++;
+    } else if (randomShell === 2) {
+        shell2.classList.add('reveal');
+        loss++;
+    } else {
+        shell3.classList.add('reveal');
+        loss++;
+    }
+    
 }); 
   // get user input
   // use user input to update state 
